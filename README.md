6a5fb894c0b8825bf07291e8efb16eda4d06505a
geoffadebonojo <geoffrey.adebonojo@gmail.com>
2021-04-03 19:47:36 -0600

    hilarious hairsplitting

--- README.md
+++ README.md
@@ -15,10 +15,21 @@ for a helpful list of commands
 
 Or jump right in and start merging arrays!
 ```
-docker run geoffadebonojo/array_sorter [1, 2, 7, 9] [3, 6, 8]
+docker run geoffadebonojo/array_sorter [1,2,7,9] [3,6,8]
 > [1, 2, 3, 6, 7, 8, 9]
 ```
 
 # Important note!
+Zsh and Bash handle command-line arguments differently!
 Zsh is wonderful but anything less that 5.5 is unable to parse an array passed as an
-argument. Run `docker run geoffadebonojo/array_sorter --params` for more detail
+argument, so you'll have to enter your arrays as strings, like so:
+```
+docker run geoffadebonojo/array_sorter "[1, 2, 7, 9] [3, 6, 8]"
+```
+
+Conversely, if you pass raw arrays to Bash, they must be free of commas, as in:
+```
+docker run geoffadebonojo/array_sorter [1,2,7,9] [3,6,8]
+```
+
+Run `docker run geoffadebonojo/array_sorter --params` for more details
--- command_line_output.rb
+++ command_line_output.rb
@@ -15,28 +15,46 @@ class CommandLineOutput
     """
   end
 
+  def self.redden(str)
+    print str.red
+  end
+
   def self.acceptable_args_example
-    acceptable = "Acceptable (any number of arrays):".green
-    unacceptable = "Unacceptable:".red
-    zsh = "zsh < 5.5".yellow
-    bash = "bash".yellow
+    acceptable = "Acceptable".green
+    unacceptable = "Unacceptable".red
+    zsh = "ZSH".yellow
+    bash = "BASH".yellow
+    q = '"'.green
+    l = "[".red
+    r = "]".red
+    c = ",".red
 
     """
-    -------------------------------------
+    --------------#{zsh}--------------------
     #{acceptable}
-    Multiple strings   => \"[1,2,3]\" \"[2,3,4]\"
-    Single string      => \"[1,2,3] [2,3,4]\"
-    Multiple arrays    =>  [1,2,3] [2,3,4]
-
+    Multiple strings                 => #{q}[1,2,3]#{q} #{q}[2,3,4]#{q}
+    Single string                    => #{q}[1,2,3] [2,3,4]#{q}
+    Spaces OK                        => #{q}[1, 2, 3], [2, 3, 4]#{q}
     #{unacceptable}
-    Multiple strings   => \"[1,2,3]\", \"[2,3,4]\"
-    Single string      => \"[1,2,3], [2,3,4]\"
-    Multiple arrays    => [1,2,3], [2,3,4]
-    Individual strings => [\"1\", \"2\", \"3\"], [\"2, 3, 4\"]
+    Commas between args BAD          => \"[1,2,3]\"#{c} \"[2,3,4]\"
+    Arrays BAD                       =>  #{l}1,2,3#{r} #{l}2,3,4#{r}
+    --------------------------------------
+
+    --------------#{bash}--------------------
+    #{acceptable}
+    Multiple strings                 => \"[1,2,3]\" \"[2,3,4]\"
+    Single string                    => \"[1,2,3] [2,3,4]\"
+    Multiple arrays                  =>  [1,2,3] [2,3,4]
+    #{unacceptable}
+    Commas between args BAD          => \"[1,2,3]\"#{c} \"[2,3,4]\"
+    Commas between args BAD          =>  [1,2,3]#{c} [2,3,4]
+    Spaces BAD                       =>  #{'[1, 2, 3] [2, 3, 4]'.red}
     --------------------------------------
 
     NOTE: If you're using #{zsh} you won't be able to parse arguments
     passed as arrays. Use string syntax. Or run the commands using #{bash}.
+    But be aware that if you pass raw arrays to bash, they must be 
+    free of spaces.
     """
   end
 
